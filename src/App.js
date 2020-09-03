0af703a3920776984a0751c5a00b752bce43560f
Theri Kay <therka@gmail.com>
2020-09-03 13:33:52 +0100

    installed axios and loader

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello World</h1>
-        <Weather />
+        <h1>Hello World!!! </h1>
+        <Weather city ="madrid"/>
       </header>
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -1,13 +1,19 @@
-import React from "react"
+import React from "react";
+import axios from "axios";
+import Loader from "react-loader-spinner";
 
-export default function Weather() {
-    
-    return (
-     <h2>
-        Hello from Weather
-    </h2>
-       
-    )
-
-
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(
+      `The weather in ${response.data.name} is ${response.data.main.temp}°C`
+    );
+  }
+  let apiKey = "1544e67f9b685a7d927f11b2e914bf96";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+  axios.get(url).then(handleResponse);
+    return <Loader
+        type="Rings"
+        color="#00BFFF"
+        height={80}
+        width={80} />;
 }
