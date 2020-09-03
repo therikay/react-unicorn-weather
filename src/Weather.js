915a121a118b758eb1cdbb51e6c1436068eb68ce
Theri Kay <therka@gmail.com>
2020-09-03 13:52:09 +0100

    changed loader color

--- src/Weather.js
+++ src/Weather.js
@@ -11,9 +11,5 @@ export default function Weather(props) {
   let apiKey = "1544e67f9b685a7d927f11b2e914bf96";
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
   axios.get(url).then(handleResponse);
-    return <Loader
-        type="Rings"
-        color="#00BFFF"
-        height={80}
-        width={80} />;
+  return <Loader type="Rings" color="#FFF" height={80} width={80} />;
 }
