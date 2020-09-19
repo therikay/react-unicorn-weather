0f6eb417c3a53ebe6a2cb6add3c43cef1cb27353
Theri Kay <therka@gmail.com>
2020-09-19 17:18:37 +0100

    minor layout edits
    Weather icon disappeared, still missing

--- src/Temperature.js
+++ src/Temperature.js
@@ -5,36 +5,36 @@ export default function Temperature(props) {
 
   function displayCelsius(event) {
     event.preventDefault();
-      setUnit("celsius");
+    setUnit("celsius");
   }
-function displayFahrenheit(event) {
-      event.preventDefault();
-      setUnit("fahrenheit");
-    }
+  function displayFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
 
-    function fahrenheit() {
+  function fahrenheit() {
     return (props.celsius * 9) / 5 + 32;
-}
+  }
 
   if (unit === "celsius") {
-     return (
+    return (
       <div className="Temperature">
         <div className="row">
-          <div className="col-5">
+          <div className="col-10">
             <div className="currentTemperature">
               <div>
                 {" "}
-                <h1 className="current-temp"> {props.celsius} </h1>{" "}
+                <h1 className="current-temp">{props.celsius}</h1>{" "}
               </div>
             </div>
           </div>
-          <div className="col-1">
+          <div className="col-2">
             <p className="units">
-              <span className="celsius"> C°</span>{" "}
+              <span className="celsius"> °C </span>{" "}
               <a href="/" className="fahrenheit" onClick={displayFahrenheit}>
-                F°
+                °F
               </a>
-            </p>
+            </p>{" "}
           </div>
         </div>
       </div>
@@ -43,20 +43,22 @@ function displayFahrenheit(event) {
     return (
       <div className="Temperature">
         <div className="row">
-          <div className="col-5">
+          <div className="col-10">
             <div className="currentTemperature">
               <div>
                 {" "}
-                <h1 className="current-temp">{Math.round(fahrenheit())} </h1>{" "}
+                <h1 className="current-temp">
+                  {Math.round(fahrenheit())}{" "}
+                </h1>{" "}
               </div>
             </div>
           </div>
-          <div className="col-1">
+          <div className="col-2">
             <p className="units">
-              <a href="/" onClick= {displayCelsius} >
-                C°
+              <a href="/" onClick={displayCelsius}>
+                °C
               </a>{" "}
-              <span className="fahrenheit">F°</span>
+              <span className="fahrenheit">°F</span>
             </p>
           </div>
         </div>
