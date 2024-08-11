853d4526165c6494046f40a2b0b9d06860ca2cdf
Theri K <64497453+therikay@users.noreply.github.com>
2024-08-11 05:52:58 +0200

    Update App.js
    Changed default location

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <div className="weatherContainer">
-        <Weather defaultCity="Cardiff"/>
+        <Weather defaultCity="Zurich"/>
         </div>
       <Footer />
     </div>
